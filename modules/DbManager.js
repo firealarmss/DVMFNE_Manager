5e73c627fb90b3ff291b5bbf47dea1861f16467b
php <99303085+firealarmss@users.noreply.github.com>
2024-01-31 15:04:41 -0600

    Add SQLite support; Add user managment; add authentication to certain pages; make landing page

---
+++ modules/DbManager.js
@@ -0,0 +1,99 @@
+/**
+ * This file is part of the fne2 tg manager project.
+ *
+ * (c) 2024 Caleb <ko4uyj@gmail.com>
+ *
+ * For the full copyright and license information, see the
+ * LICENSE file that was distributed with this source code.
+ */
+
+const sqlite3 = require('sqlite3').verbose();
+const bcrypt = require('bcryptjs');
+
+class DbManager {
+    constructor(dbFilePath) {
+        this.db = new sqlite3.Database(dbFilePath, (err) => {
+            if (err) {
+                console.log('Could not connect to database', err);
+            } else {
+                console.log('Connected to database');
+            }
+        });
+    }
+
+    initialize() {
+        this.db.run(`CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        password TEXT
+    )`, (err) => {
+            if (err) {
+                console.log('Error creating the table:', err);
+            } else {
+                this.db.get(`SELECT COUNT(*) as count FROM users`, [], (err, row) => {
+                    if (row && row.count === 0) {
+                        console.log("First time database setup");
+                        const hash = bcrypt.hashSync('password', 8);
+                        this.db.run(`INSERT INTO users (username, password) VALUES (?, ?)`, ['admin', hash], (err) => {
+                            if (err) {
+                                console.log('Error creating Default admin user:', err);
+                            } else {
+                                console.log('Default admin user created');
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    editUser(userId, username, password, callback) {
+        let sql = `UPDATE users SET username = ? WHERE id = ?`;
+        let params = [username, userId];
+
+/*        if (password) {
+            const hash = bcrypt.hashSync(password, 8);
+            sql = `UPDATE users SET username = ?, password = ? WHERE id = ?`;
+            params = [username, hash, userId];
+        }*/
+
+        this.db.run(sql, params, (err) => {
+            callback(err);
+        });
+    }
+    getUser(username, callback) {
+        return this.db.get(`SELECT * FROM users WHERE username = ?`, [username], (err, row) => {
+            callback(err, row);
+        });
+    }
+
+    addUser(username, password, callback) {
+        const hash = bcrypt.hashSync(password, 8);
+        this.db.run(`INSERT INTO users (username, password) VALUES (?, ?)`, [username, hash], (err) => {
+            callback(err);
+        });
+    }
+
+    getAllUsers(callback) {
+        return this.db.all(`SELECT * FROM users`, [], (err, rows) => {
+            callback(err, rows);
+        });
+    }
+
+    deleteUser(userId, callback) {
+        this.db.run(`DELETE FROM users WHERE id = ?`, [userId], (err) => {
+            callback(err);
+        });
+    }
+    validateUser(username, password, callback) {
+        this.getUser(username, (err, user) => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                callback(null, user);
+            } else {
+                callback('Invalid username or password');
+            }
+        });
+    }
+}
+
+module.exports = DbManager;
--- modules/TgManagerServer.js
+++ modules/TgManagerServer.js
@@ -8,9 +8,11 @@
  */
 
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
 const TgRulesHandler = require('./TgRulesHandler.js');
+const DbManager = require('./DbManager');
 
 class TgManagerServer {
     constructor(config) {
@@ -18,6 +20,8 @@ class TgManagerServer {
 
         this.config = config;
         this.app = express();
+
+        this.dbManager = new DbManager("./db/users.db");
 
         this.port = config.ServerPort || 3000;
         this.name = config.name;
@@ -31,20 +35,114 @@ class TgManagerServer {
         this.app.use(express.json());
         this.app.use(bodyParser.json());
 
-        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+
+        this.app.use(session({
+            secret: 'your_secret_key',
+            resave: false,
+            saveUninitialized: true,
+            cookie: { secure: false }
+        }));
+
+        this.app.use((req, res, next) => {
+            if (req.session && req.session.user) {
+                res.locals.user = req.session.user;
+            } else {
+                res.locals.user = null;
+            }
+            next();
+        });
+
+        this.dbManager.initialize();
 
         this.app.get('/', (req, res) => {
+            res.render('index');
+        });
+
+        this.app.get('/tg_rules', this.isAuthenticated, (req, res) => {
             let tgRulesHandler = new TgRulesHandler(this.RulePath);
             tgRulesHandler.read();
             //console.log(JSON.stringify(tgRulesHandler.rules));
 
-            res.render('index', {
+            res.render('tg_rules', {
                 rules: tgRulesHandler.rules,
                 name: this.name
             });
         });
 
-        this.app.post('/writeTgRuleChanges', (req, res) => {
+        this.app.post('/auth', (req, res) => {
+            const { username, password } = req.body;
+            this.dbManager.validateUser(username, password, (err, user) => {
+                if (user) {
+                    req.session.user = user;
+                    res.redirect('/');
+                } else {
+                    res.render('login', { message: 'Invalid username or password' });
+                }
+            });
+        });
+
+        this.app.get('/login', (req, res) => {
+            res.render('login', { message: null});
+        });
+
+        this.app.get('/logout', (req, res) => {
+            req.session.destroy();
+            res.redirect('/');
+        });
+
+        this.app.get('/users', this.isAuthenticated, (req, res) => {
+            this.dbManager.getAllUsers((err, users) => {
+                if (err) {
+                    res.send("Error retrieving users");
+                } else {
+                    res.render('userManagment', { users: users });
+                }
+            });
+        });
+
+        this.app.post('/deleteUser', this.isAuthenticated, (req, res) => {
+            this.dbManager.deleteUser(req.body.id, (err) => {
+                if (err) {
+                    res.send("Error deleting user");
+                } else {
+                    res.redirect('/users');
+                }
+            });
+        });
+
+        this.app.post('/addUser', this.isAuthenticated, (req, res) => {
+            const { username, password } = req.body;
+
+            this.dbManager.getUser(username, (err, user) => {
+                if (user) {
+                    res.redirect('/users?error=User already exists');
+                } else {
+                    this.dbManager.addUser(username, password, (err) => {
+                        if (err) {
+                            console.log('Error adding user:', err);
+                            res.redirect('/users?error=Error adding user');
+                        } else {
+                            res.redirect('/users?success=User added successfully');
+                        }
+                    });
+                }
+            });
+        });
+
+        this.app.post('/editUser', this.isAuthenticated, (req, res) => {
+            const { id, username, password } = req.body;
+            this.dbManager.editUser(id, username, password, (err) => {
+                if (err) {
+                    console.log('Error editing user:', err);
+                    res.redirect('/users?error=Error editing user');
+                } else {
+                    res.redirect('/users?success=User edited successfully');
+                }
+            });
+        });
+
+        this.app.post('/writeTgRuleChanges', this.isAuthenticated, (req, res) => {
             let rules = req.body;
             let tgRulesHandler = new TgRulesHandler(this.RulePath);
 
@@ -59,6 +157,14 @@ class TgManagerServer {
         this.app.listen(this.port, this.ServerBindAddress, () => {
             console.log(`${this.name} TG Manager Server started on port ${this.port}`);
         });
+    }
+
+    isAuthenticated(req, res, next) {
+        if (req.session && req.session.user) {
+            return next();
+        } else {
+            return res.redirect('/');
+        }
     }
 }
 
